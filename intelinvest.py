795c22423d699aae34dadf527c9fa5003c11ff00
winzard <max.vasenkov@gmail.com>
2021-09-18 17:50:16 +0300

    правки импорта облигаций и вывод купонов и дивидендов

--- intelinvest.py
+++ intelinvest.py
@@ -9,7 +9,7 @@ event_mapping = {
     'BONDBUY': 'Buy',
     'BONDSELL': 'Sell',
     'COUPON': 'Dividend',
-    'AMORTIZATION': 'Amortisation',
+    'AMORTIZATION': 'Amortisation', # TODO сделать импорт амортизации
     'DIVIDEND': 'Dividend',
     'MONEYDEPOSIT': 'Cash_In',
     'MONEYWITHDRAW': 'Cash_Out',
@@ -107,7 +107,7 @@ with open(report_file_name, 'r') as fp:
                     elif event['operation'] == 'BONDBUY' or event['operation'] == 'BONDSELL':
                         dest['Event'] = event_mapping[event['operation']]
                         dest['Symbol'] = event['ticker'] if not '.DE' in event['ticker'] else event['ticker'][0:-3]
-                        dest['Price'] = event['price']
+                        dest['Price'] = event['price']*event['nominal']/Decimal(100) # проценты и номинал надо учитывать
                         dest['Quantity'] = event['quantity']
                         dest['Currency'] = money['currency']
                         dest['FeeTax'] = event['fee']
@@ -125,6 +125,19 @@ with open(report_file_name, 'r') as fp:
                         dest['FeeTax'] = 0
                         dest['Exchange'] = ''
                         dest['NKD'] = ''
+                        dest['FeeCurrency'] = ''
+                        print(",".join([str(f) for f in dest.values()]), file=output)
+                        del buffer[source['link']]
+                    elif event['operation'] == 'COUPON' or event['operation'] == 'DIVIDEND':
+                        # связанная сделка с купоном или дивидендом - деньги зачисляются
+                        dest['Event'] = event_mapping[event['operation']]
+                        dest['Symbol'] = event['ticker'] if not '.DE' in event['ticker'] else event['ticker'][0:-3]
+                        dest['Price'] = event['price']
+                        dest['Quantity'] = money['price'] #* Decimal(0.87)  # intelinvest 13% вычитает
+                        dest['Currency'] = event['currency']
+                        dest['FeeTax'] = 0 #money['price'] * Decimal(0.13)
+                        dest['Exchange'] = ''
+                        dest['NKD'] = 0
                         dest['FeeCurrency'] = ''
                         print(",".join([str(f) for f in dest.values()]), file=output)
                         del buffer[source['link']]
@@ -157,13 +170,26 @@ with open(report_file_name, 'r') as fp:
 
                     print(",".join([str(f) for f in dest.values()]), file=output)
                 elif event['operation'] == 'COUPON' or event['operation'] == 'DIVIDEND':
+                    # несвязанная сделка - выводим сумму TODO проверить налоги
                     dest['Event'] = event_mapping[event['operation']]
                     dest['Symbol'] = event['ticker'] if not '.DE' in event['ticker'] else event['ticker'][0:-3]
                     dest['Price'] = event['price']
-                    dest['Quantity'] = event['price']*event['quantity']/Decimal(0.87) # intelinvest 13% вычитает
+                    dest['Quantity'] = event['price']*event['quantity'] # intelinvest 13% вычитает
                     dest['Currency'] = event['currency']
-                    dest['FeeTax'] = dest['Quantity']*Decimal(0.13)
-                    dest['Exchange'] = 'MCX'
+                    dest['FeeTax'] = 0 # event['price']*event['quantity']*Decimal(0.13)
+                    dest['Exchange'] = ''
                     dest['NKD'] = 0
                     dest['FeeCurrency'] = ''
+                    print(",".join([str(f) for f in dest.values()]), file=output)
+                    dest['Event'] = 'Cash_Out'
+                    # dest['Symbol'] = event['currency']
+                    dest['Price'] = 1
+                    dest['Quantity'] = event['price'] * event['quantity']
+                    # dest['Quantity'] = event['price']
+                    # dest['Currency'] = event['currency']
+                    dest['FeeTax'] = 0
+                    # dest['Exchange'] = 'MCX'
+                    dest['NKD'] = 0
+                    dest['FeeCurrency'] = ''
+
                     print(",".join([str(f) for f in dest.values()]), file=output)
